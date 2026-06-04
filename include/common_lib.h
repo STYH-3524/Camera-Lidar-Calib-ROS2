f345d9c8124a85ac0450f5973a8876a60b72a856
STYH-3524 <situyouhao@gmail.com>
2026-06-04 16:36:29 +0800

    0604

--- include/common_lib.h
+++ include/common_lib.h
@@ -14,6 +14,7 @@ which is included as part of this source code package.
 #include <pcl/point_types.h>
 #include <pcl/common/centroid.h>
 #include <cmath>
+#include <Eigen/Eigenvalues>
 
 #include "color.h"
 
@@ -287,6 +288,87 @@ void saveCalibrationResults(const Params &params, const Eigen::Matrix4f &transfo
   imwrite(outputDir + "qr_detect.png", img_input);
 }
 
+// void sortPatternCenters(pcl::PointCloud<pcl::PointXYZ>::Ptr pc,
+//                         pcl::PointCloud<pcl::PointXYZ>::Ptr v,
+//                         const std::string &axis_mode = "camera")
+// {
+//   if (pc->size() != 4)
+//   {
+//     std::cerr << "Number of centers must be 4\n";
+//     return;
+//   }
+
+//   pcl::PointCloud<pcl::PointXYZ>::Ptr work_pc(new pcl::PointCloud<pcl::PointXYZ>());
+
+//   // Coordinate transformation (LiDAR -> Camera)
+//   if (axis_mode == "lidar")
+//   {
+//     for (const auto &p : *pc)
+//     {
+//       pcl::PointXYZ pt;
+//       pt.x = -p.y; // LiDAR Y -> Cam -X
+//       pt.y = -p.z; // LiDAR Z -> Cam -Y
+//       pt.z = p.x;  // LiDAR X -> Cam Z
+//       work_pc->push_back(pt);
+//     }
+//   }
+//   else
+//   {
+//     *work_pc = *pc;
+//   }
+
+//   // --- Sorting based on the local coordinate system of the pattern ---
+//   // 1. Calculate the centroid of the points
+//   Eigen::Vector4f centroid;
+//   pcl::compute3DCentroid(*work_pc, centroid);
+//   pcl::PointXYZ ref_origin(centroid[0], centroid[1], centroid[2]);
+
+//   // 2. Project points to the XY plane relative to the centroid and calculate angles
+//   std::vector<std::pair<float, int>> proj_points;
+//   for (size_t i = 0; i < work_pc->size(); ++i)
+//   {
+//     const auto &p = work_pc->points[i];
+//     Eigen::Vector3f rel_vec(p.x - ref_origin.x, p.y - ref_origin.y, p.z - ref_origin.z);
+//     proj_points.emplace_back(atan2(rel_vec.y(), rel_vec.x()), i);
+//   }
+
+//   // 3. Sort points based on the calculated angle
+//   std::sort(proj_points.begin(), proj_points.end());
+
+//   // 4. Output the sorted points into the result vector 'v'
+//   v->resize(4);
+//   for (int i = 0; i < 4; ++i)
+//   {
+//     (*v)[i] = work_pc->points[proj_points[i].second];
+//   }
+
+//   // 5. Verify the order (ensure it's counter-clockwise) and fix if necessary
+//   const auto &p0 = v->points[0];
+//   const auto &p1 = v->points[1];
+//   const auto &p2 = v->points[2];
+//   Eigen::Vector3f v01(p1.x - p0.x, p1.y - p0.y, 0);
+//   Eigen::Vector3f v12(p2.x - p1.x, p2.y - p1.y, 0);
+//   if (v01.cross(v12).z() > 0)
+//   {
+//     std::swap((*v)[1], (*v)[3]);
+//   }
+
+//   // 6. If the original input was in the lidar frame, transform the sorted points back
+//   if (axis_mode == "lidar")
+//   {
+//     for (auto &point : v->points)
+//     {
+//       float x_new = point.z;  // Cam Z -> LiDAR X
+//       float y_new = -point.x; // Cam -X -> LiDAR Y
+//       float z_new = -point.y; // Cam -Y -> LiDAR Z
+//       point.x = x_new;
+//       point.y = y_new;
+//       point.z = z_new;
+//     }
+//   }
+// }
+
+
 void sortPatternCenters(pcl::PointCloud<pcl::PointXYZ>::Ptr pc,
                         pcl::PointCloud<pcl::PointXYZ>::Ptr v,
                         const std::string &axis_mode = "camera")
@@ -297,75 +379,54 @@ void sortPatternCenters(pcl::PointCloud<pcl::PointXYZ>::Ptr pc,
     return;
   }
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr work_pc(new pcl::PointCloud<pcl::PointXYZ>());
-
-  // Coordinate transformation (LiDAR -> Camera)
-  if (axis_mode == "lidar")
-  {
-    for (const auto &p : *pc)
-    {
-      pcl::PointXYZ pt;
-      pt.x = -p.y; // LiDAR Y -> Cam -X
-      pt.y = -p.z; // LiDAR Z -> Cam -Y
-      pt.z = p.x;  // LiDAR X -> Cam Z
-      work_pc->push_back(pt);
-    }
-  }
-  else
-  {
-    *work_pc = *pc;
-  }
-
-  // --- Sorting based on the local coordinate system of the pattern ---
-  // 1. Calculate the centroid of the points
   Eigen::Vector4f centroid;
-  pcl::compute3DCentroid(*work_pc, centroid);
-  pcl::PointXYZ ref_origin(centroid[0], centroid[1], centroid[2]);
+  pcl::compute3DCentroid(*pc, centroid);
 
-  // 2. Project points to the XY plane relative to the centroid and calculate angles
+  Eigen::Matrix3f covariance_matrix;
+  pcl::computeCovarianceMatrixNormalized(*pc, centroid, covariance_matrix);
+  Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> solver(covariance_matrix);
+  
+  Eigen::Vector3f axis_U = solver.eigenvectors().col(2).normalized(); 
+  Eigen::Vector3f axis_V = solver.eigenvectors().col(1).normalized(); 
+
   std::vector<std::pair<float, int>> proj_points;
-  for (size_t i = 0; i < work_pc->size(); ++i)
+  for (size_t i = 0; i < pc->size(); ++i)
   {
-    const auto &p = work_pc->points[i];
-    Eigen::Vector3f rel_vec(p.x - ref_origin.x, p.y - ref_origin.y, p.z - ref_origin.z);
-    proj_points.emplace_back(atan2(rel_vec.y(), rel_vec.x()), i);
+    Eigen::Vector3f rel_vec(pc->points[i].x - centroid[0], 
+                            pc->points[i].y - centroid[1], 
+                            pc->points[i].z - centroid[2]);
+    float u = rel_vec.dot(axis_U);
+    float v_val = rel_vec.dot(axis_V);
+    proj_points.emplace_back(atan2(v_val, u), i);
   }
 
-  // 3. Sort points based on the calculated angle
   std::sort(proj_points.begin(), proj_points.end());
 
-  // 4. Output the sorted points into the result vector 'v'
+  pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_v(new pcl::PointCloud<pcl::PointXYZ>());
+  tmp_v->resize(4);
+  for (int i = 0; i < 4; ++i)
+  {
+    (*tmp_v)[i] = pc->points[proj_points[i].second];
+  }
+
+  int min_u_idx = 0;
+  float min_u = std::numeric_limits<float>::max();
+  for (int i = 0; i < 4; ++i)
+  {
+    Eigen::Vector3f rel_vec(tmp_v->points[i].x - centroid[0], tmp_v->points[i].y - centroid[1], tmp_v->points[i].z - centroid[2]);
+    float u = rel_vec.dot(axis_U);
+    if (u < min_u) { min_u = u; min_u_idx = i; }
+  }
+
   v->resize(4);
   for (int i = 0; i < 4; ++i)
   {
-    (*v)[i] = work_pc->points[proj_points[i].second];
+    (*v)[i] = tmp_v->points[(min_u_idx + i) % 4];
   }
+} 
 
-  // 5. Verify the order (ensure it's counter-clockwise) and fix if necessary
-  const auto &p0 = v->points[0];
-  const auto &p1 = v->points[1];
-  const auto &p2 = v->points[2];
-  Eigen::Vector3f v01(p1.x - p0.x, p1.y - p0.y, 0);
-  Eigen::Vector3f v12(p2.x - p1.x, p2.y - p1.y, 0);
-  if (v01.cross(v12).z() > 0)
-  {
-    std::swap((*v)[1], (*v)[3]);
-  }
 
-  // 6. If the original input was in the lidar frame, transform the sorted points back
-  if (axis_mode == "lidar")
-  {
-    for (auto &point : v->points)
-    {
-      float x_new = point.z;  // Cam Z -> LiDAR X
-      float y_new = -point.x; // Cam -X -> LiDAR Y
-      float z_new = -point.y; // Cam -Y -> LiDAR Z
-      point.x = x_new;
-      point.y = y_new;
-      point.z = z_new;
-    }
-  }
-}
+
 
 class Square
 {
